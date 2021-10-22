9e824f02f38fb20ce0ebf195291ff28b32b3bd88
hayoondevil <hayoondevil@gmail.com>
2021-10-22 23:09:22 +0900

    ch03_2 user session

--- small-site/order/views.py
+++ small-site/order/views.py
@@ -7,23 +7,29 @@ from rest_framework.parsers import JSONParser
 from order.serializers import ShopSerializer, MenuSerializer
 
 from order.models import Shop, Menu, Order, Orderfood
+from user.models import User
+
 
 @csrf_exempt
 def shop(request):
   if request.method == 'GET':
-        # shop = Shop.objects.all()
-        # serializer = ShopSerializer(shop, many=True)
-        # return JsonResponse(serializer.data, safe=False)
+    try:        
+      if User.objects.all().get(id=request.session['user_id']).user_type==0:      
         shop = Shop.objects.all()
         return render(request, 'order/shop_list.html', {'shop_list':shop})
+      else:
+        return render(request, "order/fail.html")
+    except:
+      return render(request, "order/fail.html")
+
 
   elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ShopSerializer(data=data)
-        if serializer.is_valid():  # json이 맞을 경우
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    data = JSONParser().parse(request)
+    serializer = ShopSerializer(data=data)
+    if serializer.is_valid():  # json이 맞을 경우
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
 def menu(request, shop):
@@ -43,30 +49,31 @@ def menu(request, shop):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
 from django.utils import timezone
 @csrf_exempt
 def order(request):
       if request.method == 'POST':
-            shop = request.POST['shop']
-            address = request.POST['address']
-            food_list = request.POST.getlist('menu')
-            order_date = timezone.now()
+        address = request.POST['address']
+        shop = request.POST['shop']        
+        food_list = request.POST.getlist('menu')
+        order_date = timezone.now()
 
-            shop_item = Shop.objects.get(pk=int(shop))
+        shop_item = Shop.objects.get(pk=int(shop))
 
-            shop_item.order_set.create(address=address, order_date=order_date, shop=int(shop))
-            # order는 DB, 즉 models에 정의 되어있으며 소문자로 써줘야 함
+        shop_item.order_set.create(address=address, order_date=order_date, shop=int(shop))
+        # order는 DB, 즉 models에 정의 되어있으며 소문자로 써줘야 함
 
-            order_item = Order.objects.get(pk = shop_item.order_set.latest('id').id)
-            # 가장 마지막 주문 가져오기
+        order_item = Order.objects.get(pk = shop_item.order_set.latest('id').id)
+        # 가장 마지막 주문 가져오기
+        
+        for food in food_list:
+              order_item.orderfood_set.create(food_name = food)
+              # Orderfood에 추가
+
+        return render(request, 'order/success.html')
+        # return 값 필수
             
-            for food in food_list:
-                  order_item.orderfood_set.create(food_name = food)
-                  # Orderfood에 추가
-
-            return render(request, 'order/success.html')
-            # return 값 필수
-            
-      elif request.method == 'GET':
-            order_list = Order.objects.all()
-            return render(request, 'order/order_list.html', {'order_list':order_list})
+      elif request.method == 'GET':        
+        order_list = Order.objects.all()
+        return render(request, 'order/order_list.html', {'order_list':order_list})
--- small-site/user/views.py
+++ small-site/user/views.py
@@ -23,3 +23,18 @@ def user(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
+@csrf_exempt
+def login(request):
+      if request.method == 'POST':
+        name = request.POST['name']
+        try:
+          request.session['user_id'] = User.objects.all().get(name=name).id       
+          return render(request, 'user/success.html')        
+        except:
+          return render(request, 'user/fail.html')        
+      
+      elif request.method == 'GET':            
+            return render(request, 'user/login.html')
+
+                    
